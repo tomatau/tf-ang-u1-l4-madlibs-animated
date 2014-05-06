dae5039cf0cf377a8f9002b91a26aeb855da511b
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-05-07 00:35:46 +0100

    two controllers and simple validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
-function MadLibController($scope) {
+function MadLibController($scope, $rootScope) {
+    $scope.show = true;
     $scope.words = {
         your_name: 'Harold',
         sweetheart: 'Mary',
@@ -23,7 +24,49 @@ function MadLibController($scope) {
         {title: 'Male', value: 'male'},
         {title: 'Female', value: 'female'}
     ];
+
+    // not necessary
+    validWords = function(){
+        valid = true;
+        $.each($scope.words, function(i, word){
+            if ( word == null || !word) { valid = false; }
+        });
+        return valid;
+    }
+
+    function resetWords(){
+        $.each($scope.words, function(i){
+            $scope.words[i] = null;
+        });
+    }
+
+    $scope.$on('start-over', function(event){
+        resetWords();
+        $scope.show = true;
+    });
+
+    $scope.submit = function(){
+        if ( validWords() ) {
+            $scope.show = false;
+            $rootScope.$broadcast('ready', $scope.words, $scope.gender);
+        }
+    }
+}
+
+function ResultController($scope, $rootScope){
+    $scope.show = false;
+    $scope.$on('ready', function(event, words, gender){
+        $scope.show = true;
+        $scope.words = words;
+        $scope.gender = gender;
+    });
+
+    $scope.reset = function(){
+        $scope.show = false;
+        $rootScope.$broadcast('start-over');
+    }
 }
 
 angular.module('MadLibs', [])
-    .controller('MadLibController', ['$scope', MadLibController]);
+    .controller('MadLibController', ['$scope', '$rootscope', MadLibController])
+    .controller('ResultController', ['$scope', '$rootscope', ResultController]);
