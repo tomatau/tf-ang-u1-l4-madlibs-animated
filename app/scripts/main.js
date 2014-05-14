e9240860826d5035d3be52773a7470ef7a14c768
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2014-05-14 16:12:05 +0100

    cleanup and moved readme out of dist

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,35 @@
-(function IIFE(){
+;(function IIFE(undefined){
+
+    var defaults = [
+        { name: 'name', word: 'Harold' },
+        { name: 'sweetheart', word: 'Mary' },
+        { name: 'noun', word: "Sandwich" },
+        { name: 'superlative', word: "Fastest" },
+        { name: 'noun2', word: "Banana" },
+        { name: 'body_part', word: "Elbow" },
+        { name: 'continuous_verb', word: "Hum-diddling" },
+        { name: 'noun3', word: "Pizza" },
+        { name: 'verb', word: "Throw" },
+        { name: 'event', word: "Moon Landing" },
+        { name: 'day', word: "Election" },
+        { name: 'verb2', word: "Jump" },
+        { name: 'verb3', word: "Roll" },
+        { name: 'place', word: "Papa joes" },
+        { name: 'time_span', word: "5 seconds" },
+        { name: 'verb4', word: "Frolic" },
+        { name: 'adverb', word: "Glumly" }
+    ];
+
+    /**
+     * Controller for display input options
+     * @param {object} $scope
+     * @param {object} $rootScope
+     * @param {object} $timeout
+     */
     function MadLibController($scope, $rootScope, $timeout) {
         $scope.show = true;
-        var defaults = [
-            { name: 'name', word: 'Harold' },
-            { name: 'sweetheart', word: 'Mary' },
-            { name: 'noun', word: "Sandwich" },
-            { name: 'superlative', word: "Fastest" },
-            { name: 'noun2', word: "Banana" },
-            { name: 'body_part', word: "Elbow" },
-            { name: 'continuous_verb', word: "Hum-diddling" },
-            { name: 'noun3', word: "Pizza" },
-            { name: 'verb', word: "Throw" },
-            { name: 'event', word: "Moon Landing" },
-            { name: 'day', word: "Election" },
-            { name: 'verb2', word: "Jump" },
-            { name: 'verb3', word: "Roll" },
-            { name: 'place', word: "Papa joes" },
-            { name: 'time_span', word: "5 seconds" },
-            { name: 'verb4', word: "Frolic" },
-            { name: 'adverb', word: "Glumly" },
-            { name: 'pointless', word: "NotUsed" }
-        ];
 
+        // delay loading model data for loading animation
         $timeout(function(){
             $scope.words = defaults;
         });
@@ -31,24 +39,6 @@
             {title: 'Female', value: 'female'}
         ];
 
-        // not necessary
-        validWords = function(){
-            valid = true;
-            $.each($scope.words, function(i, word){
-                if ( word.word == null || !word.word) { valid = false; }
-            });
-            return valid;
-        }
-
-        function resetWords(){
-            $.each($scope.words, function(i){
-                $scope.words[i] = {
-                    name: defaults[i].name,
-                    word: ""
-                }
-            });
-        }
-
         $scope.$on('start-over', function(event){
             resetWords();
             $scope.show = true;
@@ -57,15 +47,42 @@
         $scope.submit = function(){
             if ( validWords() ) {
                 $scope.show = false;
-                $rootScope.$broadcast('ready', $scope.words, $scope.gender);
+                $rootScope.$broadcast(
+                    'ready', $scope.words, $scope.gender
+                );
             }
         }
 
         $scope.split = function (string) {
             return string.split("_").join(" ");
         }
+
+        function resetWords(){
+            $.each(defaults, function(i, d){
+                $scope.words[i] = {
+                    name: d.name,
+                    word: "",
+                    example: d.word
+                }
+            });
+        }
+
+        function validWords(){
+            var valid = true;
+            $.each($scope.words, function(i, word){
+                if ( word.word == null || !word.word) {
+                    valid = false;
+                }
+            });
+            return valid;
+        }
     }
 
+    /**
+     * Controller for displaying the madlib
+     * @param {object} $scope
+     * @param {object} $rootScope
+     */
     function ResultController($scope, $rootScope){
         $scope.show = false;
         $scope.$on('ready', function(event, words, gender){
@@ -79,6 +96,7 @@
             $rootScope.$broadcast('start-over');
         }
 
+        // find the word from a given name
         $scope.get = function(word){
             var value;
             if ($scope.words == null) return '';
@@ -89,8 +107,15 @@
         }
     }
 
+    /**
+     * Module Definition
+     */
     angular.module('MadLibs', ['ngAnimate'])
-        .controller('MadLibController', ['$scope', '$rootScope', '$timeout', MadLibController])
-        .controller('ResultController', ['$scope', '$rootScope', ResultController]);
+        .controller('MadLibController', 
+            ['$scope', '$rootScope', '$timeout', MadLibController]
+        )
+        .controller('ResultController', 
+            ['$scope', '$rootScope', ResultController]
+        );
 
 }());
---
+++ readme.md
@@ -0,0 +1,5 @@
+## preview
+
+[http://tomatau.github.io/tf-ang-u1-l4-madlibs-animated/dist/](http://tomatau.github.io/tf-ang-u1-l4-madlibs-animated/dist/)
+
+All the code has been minified and uglified as the page is only available for demonstration :)
